4eda0f3be7a91bbfdf74168b51fdc553e061c1a7
AxelHellrider <alex.nomikos@outlook.com>
2025-09-02 23:53:53 +0300

    Portfolio Project smoothening prototype #4: Crystal Re:designed & Views Handling #5.5

--- src/views/PortfolioView.tsx
+++ src/views/PortfolioView.tsx
@@ -3,7 +3,7 @@ import type { ViewProps } from "./ViewTypes";
 
 export default function PortfolioView({ onBack }: ViewProps) {
     return (
-        <ViewShell title="Portfolio" onBack={onBack}>
+        <ViewShell onBack={onBack}>
             <div className="grid grid-row w-full grid-cols-3 gap-2">
                 <div className="p-6 ring-1 ring-white/10 rounded bg-black-100/50 flex flex-col items-center hover:bg-black-0">Element 1</div>
                 <div className="p-6 ring-1 ring-white/10 rounded bg-black-100/50 flex flex-col items-center hover:bg-black-0">Element 2</div>
